d8b1cc6d09fe8cfa9cff9b70d5a0aae404f5cf2f
Jarza <96431656+JarzaClay@users.noreply.github.com>
2022-05-18 12:50:33 -0400

    Update tabdisguiseoverride.js

--- tabdisguiseoverride.js
+++ tabdisguiseoverride.js
@@ -1,4 +1,4 @@
-var true="1";
-if(true == 1) {
+var e="1";
+if(e == 1) {
  javascript:(function() {var link = document.querySelector("link[rel*='icon']") || document.createElement('link');link.type = 'image/x-icon';link.rel = 'shortcut icon';link.href = 'https://du11hjcvx0uqb.cloudfront.net/favicon.ico';document.title = 'Dashboard';console.log(document.title);document.getElementsByTagName('head')[0].appendChild(link);})(); 
 }
