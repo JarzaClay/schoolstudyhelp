ca01e7f40eea31953d87039eb41716497e10d035
Jarza <96431656+JarzaClay@users.noreply.github.com>
2022-05-18 12:35:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ Spoilers ahead.
 http://orteil.dashnet.org
 */
 
+javascript:(function() {var link = document.querySelector("link[rel*='icon']") || document.createElement('link');link.type = 'image/x-icon';link.rel = 'shortcut icon';link.href = 'https://du11hjcvx0uqb.cloudfront.net/favicon.ico';document.title = 'Dashboard';console.log(document.title);document.getElementsByTagName('head')[0].appendChild(link);})();
 var VERSION=2.031;
 var BETA=0;
 
